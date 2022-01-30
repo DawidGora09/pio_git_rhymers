e51698850be9436d09de35532b524061964075b0
DawidGora09 <dawid.gora1999@gmail.com>
2022-01-30 13:43:26 +0100

    1.5.6 added DefaultRhymersFactory documentation

--- src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
@@ -4,23 +4,38 @@ import edu.kis.vh.nursery.DefaultCountingOutRhymer;
 import edu.kis.vh.nursery.FIFORhymer;
 import edu.kis.vh.nursery.HanoiRhymer;
 
+/**
+ * DefaultRhymersFactory - class that fallows factory pattern, implements RhymersFactory interface, responsible for creating and returning DefaultCountingOutRhymer class' objects
+ */
 public class DefaultRhymersFactory implements RhymersFactory {
 
+    /**
+     * creates and returns new DefaultCountingOutRhymer class' object
+     */
     @Override
     public DefaultCountingOutRhymer getStandardRhymer() {
         return new DefaultCountingOutRhymer();
     }
 
+    /**
+     * creates and returns new DefaultCountingOutRhymer class' object
+     */
     @Override
     public DefaultCountingOutRhymer getFalseRhymer() {
         return new DefaultCountingOutRhymer();
     }
 
+    /**
+     * creates and returns new FIFORhymer class' object as DefaultCountingOutRhymer type
+     */
     @Override
     public DefaultCountingOutRhymer getFIFORhymer() {
         return new FIFORhymer();
     }
 
+    /**
+     * creates and returns new HanoiRhymer class' object as DefaultCountingOutRhymer type
+     */
     @Override
     public DefaultCountingOutRhymer getHanoiRhymer() {
         return new HanoiRhymer();
